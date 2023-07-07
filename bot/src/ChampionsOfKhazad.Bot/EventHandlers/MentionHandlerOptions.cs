62f6df0c48240f1feb69826079e5f3174033519e
Dave Pringle <davepringle46@yahoo.co.uk>
2023-07-07 19:50:27 +0200

    bot: tidy up channel-specific leftovers

--- bot/src/ChampionsOfKhazad.Bot/EventHandlers/MentionHandlerOptions.cs
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/MentionHandlerOptions.cs
@@ -1,15 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ChampionsOfKhazad.Bot;
-
-// TODO: Generify channel-specific handlers
+﻿namespace ChampionsOfKhazad.Bot;
 
 public class MentionHandlerOptions
 {
     public const string Key = "Mention";
-
-    [Required]
-    public required ulong ChannelId { get; init; }
 
     public IReadOnlyDictionary<ulong, ushort>? HourlyUserQuotas { get; init; }
 }

3b3d606f61cd3b07d29c985f2f8bf9117aea598e
Dave Pringle <davepringle46@yahoo.co.uk>
2023-07-18 22:54:11 +0200

    fix empty messages triggering question marks

--- bot/src/ChampionsOfKhazad.Bot/EventHandlers/Reactors/QuestionMarkReactor.cs
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/Reactors/QuestionMarkReactor.cs
@@ -8,7 +8,8 @@ public class QuestionMarkReactor : GuildMessageReactor
     public QuestionMarkReactor(IOptions<QuestionMarkReactorOptions> options)
         : base(options.Value.UserId, new Emoji("❔"), new Emoji("❓")) { }
 
-    protected override bool ShouldReact(IUserMessage message) => message.Content.All(x => x == '?');
+    protected override bool ShouldReact(IUserMessage message) =>
+        message.Content.Length > 0 && message.Content.All(x => x == '?');
 
     protected override Task AfterReactingAsync(IUserMessage message) =>
         message.ReplyAsync(message.Content);

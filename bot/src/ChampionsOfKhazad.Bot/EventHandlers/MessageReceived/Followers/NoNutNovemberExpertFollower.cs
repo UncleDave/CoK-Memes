1fd0ca6e02d8a5084d9ceb035e4cecd5f708c5c8
Dave Pringle <davepringle46@yahoo.co.uk>
2023-09-28 22:17:49 +0200

    bot: allow multiple trigger words in follower

--- bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/NoNutNovemberExpertFollower.cs
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/NoNutNovemberExpertFollower.cs
@@ -7,7 +7,7 @@ public record NoNutNovemberExpertFollowerOptions : BaseFollowerOptions
     public const string Key = "NoNutNovemberExpert";
 }
 
-public class NoNutNovemberExpertFollower : RandomChanceTriggerWordFollower
+public class NoNutNovemberExpertFollower : RandomChanceTriggerWordsFollower
 {
     public NoNutNovemberExpertFollower(
         IOptions<AllFollowersOptions> allFollowersOptions,
@@ -21,7 +21,7 @@ public class NoNutNovemberExpertFollower : RandomChanceTriggerWordFollower
                 allFollowersOptions.Value.IgnoreBotMentionsInChannelId,
                 $"You are Professor Jebediah Hammernut, the world's foremost expert on No Nut November (NNN). You have published several acclaimed studies on the matter, and have found that NNN is both physically and mentally detrimental, eventually leading to No Nut Psychosis. Symptoms of No Nut Psychosis include believing that your voice is deeper during NNN, and making frequent spelling mistakes. Reply to {options.Value.UserName}'s messages and attempt to educate {options.Value.UserName} on the dangers of NNN, and will try to convince them not to participate in the challenge this year (it is currently {DateTime.UtcNow:MMMM}). {options.Value.UserName} cannot respond to your message, so do not ask questions.",
                 options.Value.Chance,
-                "nnn"
+                new[] { "nnn", "no nut november" }
             ),
             assistant,
             botContext
--- bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/RandomChanceTriggerWordsFollower.cs
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/RandomChanceTriggerWordsFollower.cs
@@ -7,19 +7,19 @@ public record RandomChanceTriggerWordFollowerOptions(
     ulong IgnoreBotMentionsInChannelId,
     string Instructions,
     ushort Chance,
-    string TriggerWord
+    string[] TriggerWords
 ) : RandomChanceFollowerOptions(Target, IgnoreBotMentionsInChannelId, Instructions, Chance);
 
-public abstract class RandomChanceTriggerWordFollower : RandomChanceFollower
+public abstract class RandomChanceTriggerWordsFollower : RandomChanceFollower
 {
     private readonly RandomChanceTriggerWordFollowerOptions _options;
 
-    protected RandomChanceTriggerWordFollower(RandomChanceTriggerWordFollowerOptions options, Assistant assistant, BotContext botContext)
+    protected RandomChanceTriggerWordsFollower(RandomChanceTriggerWordFollowerOptions options, Assistant assistant, BotContext botContext)
         : base(options, assistant, botContext)
     {
         _options = options;
     }
 
     protected override bool ShouldTrigger(IUserMessage message) =>
-        message.Content.ToLowerInvariant().Contains(_options.TriggerWord) && base.ShouldTrigger(message);
+        _options.TriggerWords.Any(x => message.CleanContent.ToLowerInvariant().Contains(x)) && base.ShouldTrigger(message);
 }

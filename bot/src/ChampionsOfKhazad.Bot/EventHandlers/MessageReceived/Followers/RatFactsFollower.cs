7faa058e7ba032c15b071071227ffefde9962c86
Dave Pringle <davepringle46@yahoo.co.uk>
2023-12-07 23:32:19 +0100

    bot: rewrote rat facts prompt

--- bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/RatFactsFollower.cs
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/RatFactsFollower.cs
@@ -8,17 +8,21 @@ public record RatFactsFollowerOptions : BaseFollowerOptions
     public const string Key = "RatFacts";
 }
 
-public class RatFactsFollower(IOptions<AllFollowersOptions> allFollowersOptions,
-        IOptions<RatFactsFollowerOptions> options,
-        Assistant assistant,
-        BotContext botContext,
-        ILogger<RatFactsFollower> logger)
-    : RandomChanceFollower(new RandomChanceFollowerOptions(
-        options.Value.ToFollowerTarget(),
-        allFollowersOptions.Value.IgnoreBotMentionsInChannelId,
-        $"{options.Value.UserName} is a rat. Provide {options.Value.UserName} with a fun rat fact. If possible, the fact should relate to {options.Value.UserName}'s messages. Mention {options.Value.UserName}'s rat-like qualities and do not be kind.",
-        options.Value.Chance
-    ),
-    assistant,
-    botContext,
-    logger);
+public class RatFactsFollower(
+    IOptions<AllFollowersOptions> allFollowersOptions,
+    IOptions<RatFactsFollowerOptions> options,
+    Assistant assistant,
+    BotContext botContext,
+    ILogger<RatFactsFollower> logger
+)
+    : RandomChanceFollower(
+        new RandomChanceFollowerOptions(
+            options.Value.ToFollowerTarget(),
+            allFollowersOptions.Value.IgnoreBotMentionsInChannelId,
+            $"You are a zoologist specialising in rats. For the past 6 months you have been studying the rat-like qualities of {options.Value.UserName}. You have concluded {options.Value.UserName} behaves in a way that is almost indistinguishable from an actual rat. Consider the message from {options.Value.UserName}, and deliver them some conclusions from your study. You do not like {options.Value.UserName} due to their rat-like qualities, and should not be kind.",
+            options.Value.Chance
+        ),
+        assistant,
+        botContext,
+        logger
+    );

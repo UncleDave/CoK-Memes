3db69764cb91ca7c5ee4dc5864cbd73bc3335fc1
Dave Pringle <davepringle46@yahoo.co.uk>
2024-03-16 14:33:44 +0100

    add DiscordConsole test application

---
+++ bot/src/DiscordConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Rest;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets<Program>().Build();
+var guildId = ulong.Parse(configuration["GuildId"]!);
+var botToken = configuration["BotToken"]!;
+
+var client = new DiscordRestClient();
+await client.LoginAsync(TokenType.Bot, botToken);
+
+var guild = await client.GetGuildAsync(guildId);
+
+return;
+
+async Task<RestMessage> GetMessage(RestGuild g, ulong channelId, ulong messageId)
+{
+    var channel = await g.GetTextChannelAsync(channelId);
+    var message = await channel.GetMessageAsync(messageId);
+
+    return message;
+}

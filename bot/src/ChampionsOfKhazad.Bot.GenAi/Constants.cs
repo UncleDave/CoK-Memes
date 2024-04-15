7493dd2418aeb2709e11ee5aa73207f02ad3a5bc
Dave Pringle <DavePringle46@yahoo.co.uk>
2024-04-15 14:28:35 +0200

    switch to gpt-4-turbo

--- bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
+++ bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
@@ -4,6 +4,6 @@ public static class Constants
 {
     public const string LorekeeperName = "Lorekeeper Hammerstone";
 
-    internal const string DefaultCompletionsModel = "gpt-4-turbo-preview";
+    internal const string DefaultCompletionsModel = "gpt-4-turbo";
     internal const string DefaultEmbeddingModel = "text-embedding-3-small";
 }

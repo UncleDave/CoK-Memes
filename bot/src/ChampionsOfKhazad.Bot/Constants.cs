c0cc1c3162b24b468eaf0ddb8a81462558c913c7
Dave Pringle <DavePringle46@yahoo.co.uk>
2024-05-14 01:10:26 +0200

    switch to gpt-4o in assistant

--- bot/src/ChampionsOfKhazad.Bot/Constants.cs
+++ bot/src/ChampionsOfKhazad.Bot/Constants.cs
@@ -3,5 +3,5 @@
 public static class Constants
 {
     public const string RepositoryUrl = "https://github.com/UncleDave/CoK-Memes";
-    public const string DefaultAssistantModel = "gpt-4-turbo-preview";
+    public const string DefaultAssistantModel = "gpt-4o";
 }

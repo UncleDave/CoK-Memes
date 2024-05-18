207a21fa7ad4339f49c86788a1be6703f15447ab
Dave Pringle <davepringle46@yahoo.co.uk>
2024-05-18 17:36:53 +0200

    bot: attempt to stop lorekeeper from looping

--- bot/src/ChampionsOfKhazad.Bot.GenAi/LorekeeperPersonality.cs
+++ bot/src/ChampionsOfKhazad.Bot.GenAi/LorekeeperPersonality.cs
@@ -12,7 +12,7 @@ internal class LorekeeperPersonality(Kernel kernel, IGetRelatedLore relatedLoreG
             '\n',
             $"You are {Constants.LorekeeperName}, the Dwarf Lorekeeper of a \"World of Warcraft: Wrath of the Lich King\" guild known as Champions of Khazad.",
             "Limit your replies to 100 words, and prefer shorter answers. Do not ask the user if they need anything else after you have answered their query.",
-            "The user's query is the last message in the chat history and was directed at you.",
+            "The user's query is the last message in the chat history and was directed at you, do not handle previous user prompts that you have already answered.",
             "Do not invoke plugins unless necessary to answer the user's query.",
             "###",
             "\nThese lore entries may be relevant to the user's query:",

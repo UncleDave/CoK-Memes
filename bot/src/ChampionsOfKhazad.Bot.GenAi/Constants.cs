7e803415dabc7e83e6e06f5ab7519fb125ed567b
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2024-08-16 10:56:08 +0200

    bot: change default model to gpt-4o-2024-08-06

--- bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
+++ bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
@@ -4,6 +4,6 @@ public static class Constants
 {
     public const string LorekeeperName = "Lorekeeper Hammerstone";
 
-    internal const string DefaultCompletionsModel = "gpt-4o";
+    internal const string DefaultCompletionsModel = "gpt-4o-2024-08-06";
     internal const string DefaultEmbeddingModel = "text-embedding-3-small";
 }
--- bot/src/ChampionsOfKhazad.Bot/Constants.cs
+++ bot/src/ChampionsOfKhazad.Bot/Constants.cs
@@ -3,5 +3,5 @@
 public static class Constants
 {
     public const string RepositoryUrl = "https://github.com/UncleDave/CoK-Memes";
-    public const string DefaultAssistantModel = "gpt-4o";
+    public const string DefaultAssistantModel = "gpt-4o-2024-08-06";
 }

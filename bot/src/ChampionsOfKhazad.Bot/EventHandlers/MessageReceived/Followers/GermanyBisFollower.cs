c0bf46978838ac0e4acc3c2af0375f081a68e700
Dave Pringle <davepringle46@yahoo.co.uk>
2025-02-13 01:20:09 +0100

    bot: refactor GermanyBisFollower to support multiple user IDs and add AnyOfFollowerTriggerStrategy

--- bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/GermanyBisFollower.cs
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/GermanyBisFollower.cs
@@ -8,14 +8,14 @@ public record GermanyBisFollowerOptions
     public const string Key = "GermanyBis";
 
     [Required]
-    public ulong UserId { get; init; }
+    public required IEnumerable<ulong> UserIds { get; init; }
 }
 
 public class GermanyBisFollower(IOptions<AllFollowersOptions> allFollowersOptions, IOptions<GermanyBisFollowerOptions> options, BotContext botContext)
     : StrategyFollower(
         allFollowersOptions.Value.IgnoreBotMentionsInChannelId,
         new AllOfFollowerTriggerStrategy(
-            new TargetUserFollowerTriggerStrategy(options.Value.UserId, 198167155219365888),
+            new TargetUserFollowerTriggerStrategy(options.Value.UserIds),
             new TriggerWordFollowerTriggerStrategy("germany bis"),
             new CooldownFollowerTriggerStrategy("GermanyBis", TimeSpan.FromHours(1))
         ),
---
+++ bot/src/ChampionsOfKhazad.Bot/EventHandlers/MessageReceived/Followers/Strategies/Trigger/AnyOfFollowerTriggerStrategy.cs
@@ -0,0 +1,6 @@
+﻿namespace ChampionsOfKhazad.Bot;
+
+public class AnyOfFollowerTriggerStrategy(params IEnumerable<IFollowerTriggerStrategy> strategies) : IFollowerTriggerStrategy
+{
+    public bool ShouldTrigger(MessageReceived notification) => strategies.Any(x => x.ShouldTrigger(notification));
+}

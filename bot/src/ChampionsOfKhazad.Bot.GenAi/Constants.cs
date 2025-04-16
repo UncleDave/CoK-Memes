2fa4365ea3ca2b085cc6a43ee28dbf2709c6b3aa
Dave Pringle <DavePringle46@yahoo.co.uk>
2025-04-16 12:04:04 +0200

    bot: update completions model to gpt-4.1

--- bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
+++ bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
@@ -5,6 +5,6 @@ public static class Constants
     public const string LorekeeperName = "Lorekeeper Hammerstone";
     public const string OpenAiFriendlyLorekeeperName = "Hammerstone";
 
-    internal const string DefaultCompletionsModel = "gpt-4o-2024-08-06";
+    internal const string DefaultCompletionsModel = "gpt-4.1";
     internal const string DefaultEmbeddingModel = "text-embedding-3-small";
 }

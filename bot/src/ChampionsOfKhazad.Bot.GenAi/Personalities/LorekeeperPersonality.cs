e4513503d9cf3e967398965e55ccdc91675df38f
Dave Pringle <davepringle46@yahoo.co.uk>
2025-05-23 22:21:14 +0200

    add image generation guidelines in system prompt

--- bot/src/ChampionsOfKhazad.Bot.GenAi/Personalities/LorekeeperPersonality.cs
+++ bot/src/ChampionsOfKhazad.Bot.GenAi/Personalities/LorekeeperPersonality.cs
@@ -17,6 +17,9 @@ internal class LorekeeperPersonality(
             "Do not ask the user if they need anything else after you have answered their query.",
             "The user's query is the last message in the chat history and was directed at you, do not handle previous user prompts that you have already answered.",
             "Do not invoke plugins unless necessary to answer the user's query.",
+            "If the user asks for an image to be generated, always invoke the image generation plugin.",
+            "Each user has their own image allowance and the plugin will check if they have enough allowance to generate the image.",
+            "Do not make decisions about the user's image allowance yourself.",
             "If you perform a google search, include citations at the end of your response."
         ),
         kernel,

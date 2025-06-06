cac568fd15cb61438d284783f5d99e2cd470648a
Dave Pringle <davepringle46@yahoo.co.uk>
2025-06-06 20:28:31 +0200

    sprinkle some indexes onto the generated images collection

--- bot/src/ChampionsOfKhazad.Bot.GenAi.Mongo/MongoGenAiBotBuilderExtensions.cs
+++ bot/src/ChampionsOfKhazad.Bot.GenAi.Mongo/MongoGenAiBotBuilderExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using ChampionsOfKhazad.Bot.GenAi;
 using ChampionsOfKhazad.Bot.GenAi.Mongo;
+using ChampionsOfKhazad.Bot.Mongo;
+using MongoDB.Driver;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -8,7 +10,20 @@ public static class MongoGenAiBotBuilderExtensions
 {
     public static BotBuilder AddGenAiMongoPersistence(this BotBuilder builder)
     {
-        builder.AddMongo().AddCollection<GeneratedImage>("generatedImages").Services.AddSingleton<IGeneratedImageStore, MongoGeneratedImageStore>();
+        builder
+            .AddMongo()
+            .AddCollection<GeneratedImage>(
+                "generatedImages",
+                collection =>
+                {
+                    collection.Indexes.CreateOne(new CreateIndexModel<GeneratedImage>(Builders<GeneratedImage>.IndexKeys.Ascending(x => x.UserId)));
+                    collection.Indexes.CreateOne(
+                        new CreateIndexModel<GeneratedImage>(Builders<GeneratedImage>.IndexKeys.Ascending(x => x.Timestamp))
+                    );
+                    collection.CreateUniqueIndex(x => x.Filename);
+                }
+            )
+            .Services.AddSingleton<IGeneratedImageStore, MongoGeneratedImageStore>();
 
         return builder;
     }

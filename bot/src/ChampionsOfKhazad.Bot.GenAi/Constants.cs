f6f58cffe2b383501403072633b83e765bcb0d26
Dave Pringle <DavePringle46@yahoo.co.uk>
2025-11-22 15:48:32 +0100

    Update default completions model to gpt-5.1

--- bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
+++ bot/src/ChampionsOfKhazad.Bot.GenAi/Constants.cs
@@ -6,7 +6,7 @@ public static class Constants
     public const string OpenAiFriendlyLorekeeperName = "Hammerstone";
     public const string ImageGenerationConfirmationMessage = "Generating your image. This may take a minute.";
 
-    internal const string DefaultCompletionsModel = "gpt-4.1";
+    internal const string DefaultCompletionsModel = "gpt-5.1";
     internal const string DefaultImageModel = "gpt-image-1";
 
     internal const string GeneratedImagesBlobContainerName = "generated-images";
